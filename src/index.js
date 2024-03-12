13baead7e6bddc607cd2fac1f991957f79738a87
frckbrice <maebrie2017@gmail.com>
2024-03-12 01:15:30 +0100

    sone for the righht-card desktop

--- src/index.js
+++ src/index.js
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {RouterProvider, createBrowserRouter} from "react-router-dom"
+import Form from './components/form';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    children: [
+        {
+          path: "/login",
+          element: <Form />,
+        },
+      
+     
+    ],
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router} />
   </React.StrictMode>
 );
 
