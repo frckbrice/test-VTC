a8dd902788aebaeac7f468767a2a3c2980bfad62
frckbrice <maebrie2017@gmail.com>
2024-03-15 02:50:18 +0100

    change the min-widht in lefcard2

--- src/components/LoginCard.jsx
+++ src/components/LoginCard.jsx
@@ -1,10 +1,5 @@
-import React, { useContext,useState, useEffect  } from "react";
+import React, { useState  } from "react";
 import './LoginForm.css'
-import LoginForm from "./LoginForm";
-import LeftCard1 from "./LeftCards/LeftCard1";
-import LeftCard2 from "./LeftCards/LeftCard2";
-import LeftCard3 from "./LeftCards/LeftCard3";
-import { LogContext } from "./context";
 import LognCard1 from "./LognCard1";
 import LognCard2 from "./LognCard2";
 import LognCard3 from "./LognCard3";
